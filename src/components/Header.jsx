a8d82253705faa7c82311436f1aea0ccfdc122f6
canozzcan <canozcanstu@gmail.com>
2022-08-20 15:24:15 +0300

    Init and Add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='mainHeader'>
+        <h1>Fimple React Practicum Case One</h1>
+        <p>This app built with using React and -of course- lots of love!</p>
+    </div>
+  )
+}
+
+export default Header
